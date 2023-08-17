34ed512249454327336a67ee22ecf7ae31bac966
Bobur1545 <babajonovboburbek@gmail.com>
2023-08-17 13:29:37 +0500

    cards and userCards are created part 2

---
+++ app/Http/Controllers/PaymentCardTypeController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePaymentCardTypeRequest;
+use App\Http\Requests\UpdatePaymentCardTypeRequest;
+use App\Models\PaymentCardType;
+
+class PaymentCardTypeController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+    }
+
+    public function index()
+    {
+        return $this->response(PaymentCardType::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePaymentCardTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PaymentCardType $paymentCardType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(PaymentCardType $paymentCardType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePaymentCardTypeRequest $request, PaymentCardType $paymentCardType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PaymentCardType $paymentCardType)
+    {
+        //
+    }
+}
